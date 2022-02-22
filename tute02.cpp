27da4ac343c076d3a6a469617258fa5d82e3e5a3
IT21193736 <99173323+IT21193736@users.noreply.github.com>
2022-02-22 16:21:21 +0000

    COmplete

--- tute02.cpp
+++ tute02.cpp
@@ -1,17 +1,3 @@
-/*Exercise 2 - Selection
-
-Convert the C program given below which calculates an employee's salary to a C++ program.
-
-Input Type, Salary, otHours
-
-Type = 1
-OtRate = 1000
-Type = 2
-OtRate = 1500
-Type = 3
-OtRate = 1700
-Please Note that the input command in C++ is std::cin. This is a representation of the Keyboard.*/
-
 #include <iostream>
 using namespace std;
 
